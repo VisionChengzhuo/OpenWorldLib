349b7bc2e829728377af18112b8c6d98e7c66287
202214010807-戴亦凡 <15110540+autumnshines@user.noreply.gitee.com>
2025-11-24 17:36:55 +0800

    emu synthesis & operator & pipeline

--- test/test_emu3p5.py
+++ test/test_emu3p5.py
@@ -1,13 +1,13 @@
 import sys 
 sys.path.append("..") 
-from src.sceneflow.pipelines.emu.pipeline_emu import EmuPipeline
+from src.sceneflow.pipelines.emu.pipeline_emu3p5 import Emu3p5Pipeline
 from PIL import Image
 
 image_path = "/YOUR/IMAGE/PATH"
 model_path = "/YOUR/MODEL/PATH"
 test_prompt = "Translate this house into a school."
 
-pipeline = EmuPipeline.from_pretrained(
+pipeline = Emu3p5Pipeline.from_pretrained(
     pretrained_model_path=model_path,
     use_image=True,
 )
