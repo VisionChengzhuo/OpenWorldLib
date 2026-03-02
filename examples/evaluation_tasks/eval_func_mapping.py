84a0b3be7cdd595f5d9f194c0d08f0c507ecc2ef
dailihua <dl_h_1@126.com>
2026-03-02 15:22:56 +0800

    support eval it2v

--- examples/evaluation_tasks/eval_func_mapping.py
+++ examples/evaluation_tasks/eval_func_mapping.py
@@ -2,9 +2,9 @@ from .navigation_video_generation import (
     reference_func as nav_video_gen_ref_func,
     eval_func as nav_video_gen_eval_func,
 )
-from .text2video_generation import (
-    reference_func as text2video_gen_ref_func,
-    eval_func as text2video_gen_eval_func,
+from .imagetext2video_generation import (
+    reference_func as imagetext2video_gen_ref_func,
+    eval_func as imagetext2video_gen_eval_func,
 )
 
 
@@ -13,8 +13,8 @@ eval_func_mapping = {
         "reference_func": nav_video_gen_ref_func,
         "eval_func": nav_video_gen_eval_func
     },
-    "text2video_gen": {
-        "reference_func": text2video_gen_ref_func,
-        "eval_func": text2video_gen_eval_func
+    "imagetext2video_gen": {
+        "reference_func": imagetext2video_gen_ref_func,
+        "eval_func": imagetext2video_gen_eval_func
     }
 }
